84183d91f1833c02f92d6c2370552d915ed545be
Chris Jones <christopher.jones@apsva.us>
2025-02-26 13:46:23 -0500

    finish todo CRUD

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("<int:pk>/", views.DetailView.as_view(), name="detail"),
     #
     path("add/",views.CreateView.as_view(), name="add"),
+    path("update/<int:pk>/", views.UpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.DeleteView.as_view(), name="delete"),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django.urls import reverse, reverse_lazy
 from .models import Item 
 from .forms import ItemForm
 # Create your views here.
@@ -15,5 +15,13 @@ class DetailView(generic.DetailView):
 class CreateView(generic.edit.CreateView):
     model = Item
     fields = '__all__'
+    success_url = reverse_lazy("todo:index")
 
+class UpdateView(generic.edit.UpdateView):
+    model = Item
+    fields = '__all__'
+    success_url = reverse_lazy("todo:index")
 
+class DeleteView(generic.edit.DeleteView):
+    model = Item
+    success_url = reverse_lazy("todo:index")
